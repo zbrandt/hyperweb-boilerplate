dcf54478cb8b288038f82c8c40c7df5738807fba
Anmol1696 <anmol1696@gmail.com>
2024-10-02 15:04:57 +0400

    add contracts2

---
+++ contracts/bundle2.js
@@ -0,0 +1,28 @@
+// src/contract2/index.ts
+function reset(state) {
+  const newValue = 0;
+  state.value = 0;
+  return newValue;
+}
+function inc(state, { x }) {
+  const oldValue = state.value ?? 0;
+  const newValue = oldValue + x;
+  state.value = newValue;
+  return newValue;
+}
+function dec(state, { x }) {
+  const oldValue = state.value ?? 0;
+  const newValue = oldValue - x;
+  state.value = newValue;
+  return newValue;
+}
+function read(state) {
+  return state.value;
+}
+export {
+  dec,
+  inc,
+  read,
+  reset
+};
+//# sourceMappingURL=bundle2.js.map

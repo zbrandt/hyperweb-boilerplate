0786add789e05440a7de8c1c598f03b0d33d48b2
Anmol1696 <anmol1696@gmail.com>
2024-10-15 13:29:24 +0400

    update contract2 as well

--- src/contract1/index.ts
+++ src/contract1/index.ts
@@ -1,5 +1,5 @@
 export interface State {
-  get(key: string): any;
+  get(key: string): string;
   set(key: string, value: any): void;
 }
 
@@ -9,13 +9,13 @@ export function reset(state: State) {
   return newValue
 }
 export function inc(state: State, { x }: { x: number }) {
-  const oldValue = state.get('value') ?? 0;
+  const oldValue = Number(state.get('value')) || 0;
   const newValue = oldValue + x;
   state.set('value', newValue);
   return newValue
 }
 export function dec(state: State, { x }: { x: number }) {
-  const oldValue = state.get('value') ?? 0;
+  const oldValue = Number(state.get('value')) || 0;
   const newValue = oldValue - x;
   state.set('value', newValue);
   return newValue
